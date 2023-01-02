929f8211340d358364f5d5db67da6e5b2d55dc74
miikkaki <miikka@e-cartic.com>
2023-01-02 13:52:19 +0200

    home page

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,10 +1,11 @@
 import '../../styles/header.css';
+import { Link } from 'react-router-dom';
 
 function Header() {
     return (
         <header className="header">
             <div>
-                Helsinki City Bike App
+                <Link to="/">Helsinki City Bike App</Link>
             </div>
         </header>
     );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,11 @@
+import Bike from '../assets/bicycle.png';
+
 const Home = () => {
     return (
-        <div>Hello World</div>
+        <div className='container'>
+            <img className='home-image' src={Bike}></img>
+            <div>Welcome to Helsinki City Bike App!</div>
+        </div>
     )
 }
 
