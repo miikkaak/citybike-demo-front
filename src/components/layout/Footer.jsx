a4648be307e65db710e3f004ca5f4195c64e0d93
miikkaki <miikka@e-cartic.com>
2023-01-05 12:29:26 +0200

    improved footer

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,13 +1,18 @@
 import { Link } from 'react-router-dom';
 import '../../styles/footer.css';
+import LinkedIn from '../../assets/linkedin.svg';
+import Facebook from '../../assets/facebook.svg';
+import Twitter from '../../assets/twitter.svg';
 
 function Footer() {
     return (
         <div className="footer">
             <div className="footer-container">
                 <div className="socials">
-                    <div className="hello title-size-medium">Socials</div>
-                    {/* <div className='socials-row'><a href=''>{<LinkedIn/>}LinkedIn</a></div> */}
+                    <div className="hello title-size-medium">Social Media</div>
+                    <div className='socials-row'><a href=''><img src={LinkedIn}/>LinkedIn</a></div>
+                    <div className='socials-row'><a href=''><img src={Facebook}/>Facebook</a></div>
+                    <div className='socials-row'><a href=''><img src={Twitter}/>Twitter</a></div>
                 </div>
                 <div className="site-items">
                     <div className="hello title-size-medium">Site Links</div>
